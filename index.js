507618be4fcea91cfc6249ab94c764eedfa91286
UnitySam Rung <unitysamrung@gmail.com>
2016-07-20 23:22:00 +0700

    change

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require ('express');
+var app = express ();
+var server = require ('http').createServer (app);
+var io = require ('socket.io').listen (server);
+
+app.set ('port', process.env.PORT || 3000);
+
+app.get ('/', getRoot);
+
+server.listen (app.get ('port'), function () {
+  console.log ("Server is running")
+});
+
+var user = [] [];
+
+io.on('connection', function (socket) {
+
+  socket.on ('connect', function (r_data) {
+    user [r_data ['user']] ['socket_id'] = socket.id;
+    user [r_data ['user']] ['pos'] = r_data ['pos'];
+
+    socket.broadcast.emit ('connect', r_data);
+    user.forEach(function (index) {
+        var data = {user : user [index] ['socket_id']], position : user [index] ['position']}
+
+        socket.emit ('connect', data);
+    });
+  });
+
+  socket.on('movement', function (r_data) {
+    user [r_data ['user']] ['pos'] = r_data ['pos'];
+
+    socket.broadcast.emit ('movement', r_data);
+  });
+
+  socket.on('logout', function (r_data) {
+    socket.broadcast.emit ('logout', r_data);
+  });
+});
